6c5458bfef7ee243e45c96b1a300ac83bfda7269
Shahil Saha <71847625+shahilsaha05uk@users.noreply.github.com>
2024-05-09 15:22:37 +0100

    s

--- Assets/Scripts/Helpers/AnimationParametersDictionary.cs
+++ Assets/Scripts/Helpers/AnimationParametersDictionary.cs
@@ -3,5 +3,9 @@
     public static class AnimationParametersDictionary
     {
         public static string Trigger_Bottom_To_Center_Panel = "bTrigger";
+        
+        
+        // Zombie Animations
+        public static string Trigger_IsDead = "bIsDead";
     }
 }
